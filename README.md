a02e2f977e090621de856d74d37dd26c27e82fc9
Rory Hart <hartror@gmail.com>
2017-05-31 20:51:22 +1000

    Package setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pyramid OpenId Connect
+
+Experimental implementation of the OpenId Connect (OIDC) protocol using
+(pyoidc)[https://github.com/OpenIDC/pyoidc].
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyramid-oidc',
+    version='0.0.0',
+    description='',
+    long_description=readme,
+    author='Rory Hart',
+    author_email='hartror@gmail.com',
+    url='https://github.com/hartror/pyramid-odic',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
