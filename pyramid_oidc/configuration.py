5cc3d5cdd1788bc74f7146a30676852245df0de9
Rory Hart <hartror@gmail.com>
2017-06-10 20:35:28 +1000

    Finished client_from_settings

--- pyramid_oidc/configuration.py
+++ pyramid_oidc/configuration.py
@@ -1,23 +1,34 @@
+import logging
+
 from oic.oic import Client
 from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 
 from pyramid_oidc.exceptions import MissingConfigurationException
 
 
+log = logging.getLogger(__name__)
+
+
 def client_from_settings(settings):
     """
-    Create a oic client from a configuration dictionary.
+    Return a oic client from a configuration dictionary.
     """
-    validate_config(config)
+    validate_config(settings)
 
     client_id = settings['oidc.client_id']
     client_secret = settings['oidc.client_secret']
-    provider_uri = settings['oidc.provider_config_uri']
+    provider_config_uri = settings['oidc.provider_config_url']
 
-    client = Client(client_id=client_id)
+    client = Client(client_id=client_id, client_authn_method=CLIENT_AUTHN_METHOD)
     client.client_secret = client_secret
 
-    client.provider_config(provider_uri)
+    try:
+        client.provider_config(provider_config_uri)
+    except Exception as e:
+        log.error(
+            "Unable to load config from provider_config_url '{}'"
+            .format(provider_uri))
+        raise
 
     return client
 
@@ -25,7 +36,7 @@ def client_from_settings(settings):
 def validate_config(settings):
     missing_options = [
         option
-        for option in ('oidc.provider_uri', 'oidc.client_id', 'oidc.client_secret')
+        for option in ('oidc.provider_config_url', 'oidc.client_id', 'oidc.client_secret')
         if option not in settings]
     if missing_options:
         raise MissingConfigurationException(missing_options)
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,14 +1,36 @@
+import mock
 import pytest
 
-from pyramid_oidc.configuration import validate_config
+from pyramid_oidc.configuration import (
+    client_from_settings,
+    validate_config)
 from pyramid_oidc.exceptions import MissingConfigurationException
+
+
+@pytest.fixture
+def mock_client_cls(monkeypatch):
+    client_cls = mock.Mock(spec=[])
+    client_cls.return_value = mock.Mock(spec=['client_secret', 'provider_config'])
+    monkeypatch.setattr('pyramid_oidc.configuration.Client', client_cls)
+    return client_cls
+
+
+def test_client_from_settings(mock_client_cls):
+    settings = {
+        'oidc.client_id': 'unit_test',
+        'oidc.client_secret': 'deadbeef-dead-beef-dead-deadbeefdead',
+        'oidc.provider_config_url': 'http://example.org/'}
+
+    client = client_from_settings(settings)
+
+    assert client == mock_client_cls.return_value
 
 
 def test_valid_config():
     settings = {
         'oidc.client_id': '',
         'oidc.client_secret': '',
-        'oidc.provider_uri': ''}
+        'oidc.provider_config_url': ''}
 
     validate_config(settings)
 
